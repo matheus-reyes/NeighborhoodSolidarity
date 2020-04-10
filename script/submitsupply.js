11ed181f5fd75046502f98383ee90aa3349a657c
Matheus Reyes <matheus-reyes@usp.br>
2020-04-10 00:01:03 -0300

    Atualizando Páginas

--- script/submitsupply.js
+++ script/submitsupply.js
@@ -1,10 +1,22 @@
 firebase.initializeApp({ databaseURL: "https://neighborhood-9060d.firebaseio.com/" });
 
-async function test () {
-  const adress = '1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyAmMhKrT7FzP2csp881g__Aq47mOP1uWaM';
-  const response = await fetch('https://maps.googleapis.com/maps/api/geocode/json?address=' + adress)
+const form = document.querySelector('form');
+const adress = document.getElementById("inputAdress").value;
+
+form.onsubmit = e => {
+  e.preventDefault();
+  console.log(adress)
+  test(adress)
+}
+
+
+
+async function test (input) {
+  const apikey = 'AIzaSyAmMhKrT7FzP2csp881g__Aq47mOP1uWaM';
+  const adress = input.replace(' ', '+');
+  console.log(input);return;
+  const response = await fetch('https://maps.googleapis.com/maps/api/geocode/json?address=' + adress + apikey)
   const data = await response.json();
   console.log(data);
 }
 
-test();
